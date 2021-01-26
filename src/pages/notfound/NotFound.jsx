a44c187aad9bce6d8c9165f3ac82d8c662834e9a
Franklin-Monster <1453765616@qq.com>
2021-01-26 14:23:43 +0800

    not found

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './not-found'
+
+import ReturnTitle from '@c/return-title'
 const NotFound = () => {
     return (
         <div id="NotFound">
-            213
+            <div className="not-header">
+                <ReturnTitle arrowColor="black" background="#FAFAFA" />
+            </div>
+            <div className="not-body">
+                此页面正在开发中
+            </div>
         </div>
     )
 }
